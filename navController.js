73688a8991b07534b27e9d6d82484992e9994e72
Ryzen32 <123226234+Ryzen32@users.noreply.github.com>
2023-03-13 17:30:13 +0530

    Add files via upload

---
+++ navController.js
@@ -0,0 +1,34 @@
+const Navbar = require("../models/Navbar");
+const Product = require("../models/Product");
+
+/* ADD to NAVBAR */
+const addNavbarItem = async (req, res) => {
+  const payload = req.body;
+
+  try {
+    const newNavbar = new Navbar(payload);
+    const savedNavbarProduct = await newNavbar.save();
+    res.status(200).json({
+      message: "Product successfully added.",
+      savedNavbarProduct,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+/* GET ALL THE PRODUCTS */
+const getNavbarProducts = async (req, res) => {
+  const query = req.query.q;
+  const regex = new RegExp(query, "i");
+  try {
+    const navbars = await Product.find({
+      $or: [{ name: regex }],
+    });
+    res.status(200).json(navbars);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { addNavbarItem, getNavbarProducts };
